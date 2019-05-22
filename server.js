fc6fbe0f2a5ad4f8a1b69f15bf820ea756fd3090
lioartoil <a.chanakant@gmail.com>
2019-05-22 12:28:58 +0700

    first try

--- server.js
+++ server.js
@@ -1,37 +1,72 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 var cors = require('cors');
+const dns = require('dns');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
+let counter = 1;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+const urlSchema = mongoose.Schema({
+	original_url: { type: String, required: true },
+	short_url: { type: Number, default: 1 }
+});
+
+const Url = mongoose.model('url', urlSchema);
+
+/** this project needs a db !! **/
+mongoose.connect(
+	process.env.MONGOLAB_URI ||
+		`mongodb+srv://lioartoil:jbpr0Zow1HFtXZOH@api-and-microservices-a57mx.mongodb.net/url-shortener?retryWrites=true`,
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	}
+);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function(req, res) {
+	res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/shorturl/:shorten', async (req, res) => {
+	const url = await Url.findOne({ short_url: shorten });
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+	if (url) {
+		res.redirect(url.original_url);
+	}
+});
+
+app.post('/api/shorturl/new', (req, res) => {
+	dns.lookup(req.body.url, (err, addresses) => {
+		if (err) return;
+
+		const url = new Url({ original_url: req.body.url, short_url: counter++ });
+
+		url.save((err, data) => {
+			if (err) return res.status(400).send();
+			res.json({ original_url: url.original_url, short_url: url.short_url });
+		});
+	});
+});
+
+app.listen(port, function() {
+	console.log('Node.js listening ...');
 });
